79804523c5ebba1f6fcb6bd57c1a369d879d1a36
Marco Toliman Lucchini <mlucchin@lxplus0035.cern.ch>
2015-07-19 09:24:07 +0200

    small changes

--- generate_source_lxplus.pl
+++ generate_source_lxplus.pl
@@ -1,14 +1,15 @@
-#$baseDir = "/afs/cern.ch/work/m/mlucchin/TB_timing/jobs";
-$baseDir = ".";
+$baseDir = "/afs/cern.ch/work/m/mlucchin/TB_timing/jobs";
 $jobDir = $baseDir;
 
 
 # create job file for each configuration set
 
-$NJOBS = 10;
+$NJOBS = 100;
 
 for($iJob = 0; $iJob < $NJOBS; ++$iJob)
 {
+#    $jobDir = $baseDir."/jobdir_".$iJob;    
+#    system("mkdir ".jobDir);
     $jobFile = $jobDir."/job_".$iJob.".sh\n" ;
     system("cat ".$baseDir."/job_template.sh | sed -e s%ID%".$iJob."%g > ".$jobFile);    
 }
@@ -21,5 +22,6 @@ open(LANCIAFILE, ">", $lanciaFile);
 
 for($iJob = 0; $iJob < $NJOBS; ++$iJob)
 {
-	print LANCIAFILE "bsub -cwd /afs/cern.ch/work/m/mlucchin/TB_timing/jobs/ -q 1nh /afs/cern.ch/work/m/mlucchin/TB_timing/jobs/job_".$iJob.".sh \n"; 
+#	print LANCIAFILE "mkdir ".$baseDir."/jobdir_".$iJob." \n";
+	print LANCIAFILE "bsub -cwd /afs/cern.ch/work/m/mlucchin/TB_timing/jobs/output -q 2nd /afs/cern.ch/work/m/mlucchin/TB_timing/jobs/job_".$iJob.".sh \n"; 
 }
